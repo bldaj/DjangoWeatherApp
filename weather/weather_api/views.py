de45e580de1ac80364bd2a51835c071338d62c5f
Konstantin <wetarmor@gmail.com>
2018-10-12 14:07:47 +0300

    Handling query_params added

--- weather/weather_api/views.py
+++ weather/weather_api/views.py
@@ -1,10 +1,18 @@
 from rest_framework import viewsets
-from django.shortcuts import render
 
 from . import serializers
 from .models import City
 
 
 class WeatherViewSet(viewsets.ModelViewSet):
-    queryset = City.objects.all()
     serializer_class = serializers.WeatherSerializer
+
+    def get_queryset(self):
+        queryset = City.objects.all()
+
+        city_name = self.request.query_params.get('name', None)
+
+        if city_name is not None:
+            queryset = queryset.filter(name__contains=city_name)
+
+        return queryset
