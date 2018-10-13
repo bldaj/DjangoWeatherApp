675e732513906b0729929c1e087911037fa0518c
Konstantin <wetarmor@gmail.com>
2018-10-13 19:03:19 +0300

    Pagination added

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -132,6 +132,8 @@ CELERY_BROKER_URL = 'amqp://localhost:5672'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
