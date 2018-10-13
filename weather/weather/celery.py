f2cadb2af93da57b3578c79e18e8d11e40e31bb8
Konstantin <wetarmor@gmail.com>
2018-10-13 18:51:05 +0300

    Temperatures fields added

--- weather/weather/celery.py
+++ weather/weather/celery.py
@@ -13,7 +13,7 @@ app = Celery('weather')
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(3600.0, populate_db.s(), name='Populate every hour')
+    sender.add_periodic_task(10.0, populate_db.s(), name='Populate every hour')
 
 
 @app.task
@@ -25,7 +25,7 @@ def test_task(txt):
 def populate_db():
     from weather_api import models
 
-    url = 'http://api.openweathermap.org/data/2.5/forecast?q={0}&appid=d0068bdfb3a12607733e3b32622f9ac2'
+    url = 'http://api.openweathermap.org/data/2.5/forecast?q={0}&units=metric&appid=d0068bdfb3a12607733e3b32622f9ac2'
 
     with open('weather/cities.txt') as cities_file:
         for city in cities_file:
@@ -37,4 +37,8 @@ def populate_db():
             if city_weather_for_3_hours['cod'] != '404':
                 for weather_for_3_hour in city_weather_for_3_hours['list']:
                     dt = datetime.strptime(weather_for_3_hour['dt_txt'], '%Y-%m-%d %H:%M:%S')
-                    models.City(name=city, date=dt).save()
+                    temp = weather_for_3_hour['main']['temp']
+                    temp_min = weather_for_3_hour['main']['temp_min']
+                    temp_max = weather_for_3_hour['main']['temp_max']
+
+                    models.City(name=city, date=dt, temp=temp, temp_min=temp_min, temp_max=temp_max).save()
--- weather/weather_api/models.py
+++ weather/weather_api/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class City(models.Model):
     name = models.CharField(max_length=50, null=False, db_index=True)
     date = models.DateTimeField(null=False, db_index=True)
+    temp = models.FloatField(null=False, default=-278)
+    temp_min = models.FloatField(null=False, default=-278)
+    temp_max = models.FloatField(null=False, default=-278)
 
     def __str__(self):
         return self.name
