d06eb606fe2175b33478c991fb0f5f06843019d3
Konstantin <wetarmor@gmail.com>
2018-10-15 15:32:01 +0300

    test_get_all_weather_for_one_city Test added. test_get_all_weather_data_for_all_cities changed a little

--- weather/weather_api/tests.py
+++ weather/weather_api/tests.py
@@ -45,12 +45,45 @@ class TestWeatherApi(APITestCase):
         for city in cities:
             create_city(name=city['name'], date=city['date'])
 
-        response = self.client.get('/weather/')
-
         for city in cities:
             city['date'] = convert_datetime_to_str(city['date'])
+
+        response = self.client.get('/weather/')
 
         self.assertEqual(
             cities,
             convert_ordered_dict_to_list_of_dicts(list(response.data['results']))
         )
+
+    def test_get_all_weather_for_one_city(self):
+        cities = [
+            {
+                'name': 'andorra',
+                'date': datetime.now(),
+                'temp': -278.0,
+                'temp_min': -278.0,
+                'temp_max': -278.0
+            },
+            {
+                'name': 'aixirivall',
+                'date': datetime.now(),
+                'temp': -278.0,
+                'temp_min': -278.0,
+                'temp_max': -278.0
+            }
+        ]
+
+        for city in cities:
+            create_city(name=city['name'], date=city['date'])
+
+        cities[0]['date'] = convert_datetime_to_str(cities[0]['date'])
+
+        response = self.client.get('/weather/?name={0}'.format(cities[0]['name']))
+
+        dicts_list = convert_ordered_dict_to_list_of_dicts(list(response.data['results']))
+
+        if len(dicts_list) == 1:
+            self.assertEqual(
+                cities[0],
+                dicts_list[0]
+            )
