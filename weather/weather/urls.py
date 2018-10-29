d8bbdbbe312cfced4b62707a72aee496a5c62bb1
Konstantin <wetarmor@gmail.com>
2018-10-29 15:20:33 +0300

    Weather app routing added

--- weather/weather/urls.py
+++ weather/weather/urls.py
@@ -17,13 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from weather_api import views
+from weather_api import views as api_views
 
 router = routers.DefaultRouter()
-router.register('weather', views.WeatherViewSet, base_name='weather')
+router.register('', api_views.WeatherViewSet, base_name='weather')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include('weather_app.urls')),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls'), name='rest_framework'),
     path('admin/', admin.site.urls),
 ]
